97329f7a62af27d29d32c1dc87325ccfcc810408
Anum Qudsia <AnumQ@users.noreply.github.com>
2023-09-17 22:50:14 +0200

    Fetch stock data (demo) and display in a table

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { StockDataTable } from "./UI/ListStockData";
+import axios from "axios";
+import { StockData } from "./Models/StockData";
 
 function App() {
+  const [data, setData] = useState<StockData[] | null>(null);
+
+  async function fetchData() {
+    try {
+      const response = await axios.get(
+        "https://eodhd.com/api/eod/MCD.US?api_token=demo&period=d&fmt=json"
+      );
+
+      if (response.status !== 200) {
+        throw new Error("Network response was not 200");
+      }
+      setData(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data ? <StockDataTable data={data} /> : <p>Fetching data ...</p>}
     </div>
   );
 }
---
+++ src/UI/ListStockData.tsx
@@ -0,0 +1,39 @@
+import { StockData, StockDataProps} from "../Models/StockData";
+const renderStockData = (data: StockData[]) => {
+  if (data.length === 0) {
+    return <p>No data available</p>;
+  }
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Open</th>
+          <th>High</th>
+          <th>Low</th>
+          <th>Close</th>
+          <th>Adjusted Close</th>
+          <th>Volume</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((entry, index) => (
+          <tr key={index}>
+            <td>{entry.date}</td>
+            <td>{entry.open}</td>
+            <td>{entry.high}</td>
+            <td>{entry.low}</td>
+            <td>{entry.close}</td>
+            <td>{entry.adjusted_close}</td>
+            <td>{entry.volume}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export const StockDataTable: React.FC<StockDataProps> = ({ data }) => {
+  return <div>{renderStockData(data)}</div>;
+};
